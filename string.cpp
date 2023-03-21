d9e81a2aa407bc5380ab21242862513462db4319
wuenshin <danilmandzy@yandex.ru>
2023-03-21 20:25:29 +0300

    Добавьте файлы проекта.

---
+++ string.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+
+
+//// Задание 1
+//// Составить алгоритм, исключающий из текста “лишние” пробелы. (В тексте должен быть один пробел для разделения слов).
+//int main()
+//{
+//    int lenA, lenB, Space;
+//    std::string A = "g o od mo m en t";
+//    std::string B = " ";
+//    std::cout << "Specify which character will be followed by a space: ";
+//    std::cin >> Space;
+//    lenA = A.length();
+//    lenB = B.length();
+//    for (int c = 0; c < lenB; c++){
+//        for (int i = 0; i < lenA; i++){
+
+//            if( i == Space ){
+//                  i++;
+//            }
+
+//            if( B[c] == A[i] ){
+//               A.erase(i, 1);
+
+//            }
+
+//        }
+//    }
+//    std::cout << std::endl << A << std::endl;
+//    return 0;
+//}
+
+
+//// Задание 2
+//// Составить алгоритм, подсчитывающий количество слов в тексте.
+//int main()
+//{
+//    int lenA, lenB, S;
+//    std::string A = "Beautiful weather. I even want to go for a walk somewhere";
+//    std::string B = " ";
+//    lenA = A.length();
+//    lenB = B.length();
+//    for (int c = 0; c < lenB; c++){
+//        for (int i = 0; i < lenA; i++){
+
+//            if( B[c] == A[i] ){
+//               S++;
+
+//            }
+
+//        }
+//      S++;
+//    }
+//    std::cout << "Number of word:  " << S << std::endl;
+//    return 0;
+//}
+
+
+//// Задание 3
+//// Дана строка. Удалите k-ый символ в ней.
+//int main (){
+//    std::string Text, sim, Itog;
+//    int t;
+//    std::cout << "please, enter number: ";
+//    sim = " ";
+//    std::cin >> t;
+//    Text = "It was raining";
+//     Text.erase(t - 1, 1);
+
+//      std::cout << Text;
+//}
+
+
+//// Задание 4
+//// Исключить из строки группы символов, расположенные между символами «/*» и «*/» включая границы. Предполагается, что нет вложенных скобок.
+// int main (){
+//     std::string Text;
+//     int t, pos1, pos2;
+//     Text = "It was raining, but the day /* It was Wednesday *\ was still very beautiful";
+//     pos1 = Text.find( "/*" );
+//     pos2 = Text.find( "*\\" );
+//         t = pos2 - pos1;
+//      Text.erase(pos1, t);
+//       std::cout << Text;
+// }
